91a35b0477afbf7698df6cc3109067be3d162c7b
Danya <danyameadows@gmail.com>
2021-01-23 23:16:29 -0600

    initial file list

---
+++ R/myf.R
@@ -0,0 +1,12 @@
+#' Square Function
+#'takes a vector and returns a vector of squared componets
+#' @param x
+#'
+#' @return a vector of swuared componets
+#' @export
+#'
+#' @examples
+#' x=1:30; myf(x)
+myf = function(x){
+  x^2
+  }
